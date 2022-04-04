515d2b1253a164430ea5b31c5a454410b0d67cdb
rexnard <100132176+rexnard@users.noreply.github.com>
2022-04-04 20:53:36 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<hmtl>
+    <head>
+        <title>Poem Of the Day</title>
+        <link href="https://fonts.google.com/specimen/Beau+Rivage:wght@700&display=swap" rel="stylesheet">
+        <style>
+            .font-one{
+            font-family: 'Beau Rivage', cursive;
+            }
+            body{
+             background-image: url(background.jpg.crdownload);
+             background-repeat: no-repeat;
+             background-position: top center;
+             background-size: cover;            
+            }
+            #wrapper{
+             width: 1020px;
+             min-height: 20px;
+             margin: 0 auto;
+             padding: 10pc;  
+            }
+            #content{
+             width: 130%;
+             min-height: 30px;
+             float: left;
+             position: center;
+             bottom: 300px;
+             right: 200px;
+             font-size:60px;
+             color: wheat;
+             margin-top: 130px; 
+            
+            
+            }</style>
+    </head>
+    <body>
+        <div id= "wrapper">
+             <div id="content">
+             <p class = "font-one">
+                "I've seen better days, but I've also seen worse. I dont have everything that I want, but I do have all I need. I woke up with some aches and pains, but I woke up. My life may not be perfect, but I'm blessed."
+</p>
+            </div>
+        </div>
+    </body>
+</hmtl>
